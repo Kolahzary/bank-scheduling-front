c2f09f8989e0a92f0fe67bd36c679f4c9d622a1c
Sheyda Allahweisi <sh.allahweisi@gmail.com>
2021-01-02 21:46:45 +0330

    feat: implement server page ui

--- pages/server/_id.vue
+++ pages/server/_id.vue
@@ -1,24 +1,64 @@
 <template>
-  <div>
-    <div>Logged in as {{ name }} (server terminal #{{ id }})</div>
-    <div>
-      <span>Customers</span>
+  <div class="min-h-screen bg-gray-800">
+    <div class="w-9/12 mx-auto">
+      <!-- Title -->
+      <div class="w-full h-20 bg-gray-700 flex justify-center items-center">
+        <span
+          class="text-gray-500 font-bold text-xl"
+        >Logged in as {{ name }} (server terminal #{{ id }})</span>
+      </div>
+      <!-- Content -->
+      <div class="w-full flex">
+        <!-- Customer -->
+        <div class="w-1/2 px-16">
+          <div class="flex items-center justify-center my-8">
+            <span class="text-gray-600 font-semibold text-xl">Customers</span>
+          </div>
+          <div
+            class="w-full border-gray-600 border-2 px-6 py-4 rounded-lg mt-4"
+            v-for="customer in customers"
+            :key="customer.id"
+          >
+            <div class="w-full flex justify-between items-center">
+              <div class="flex items-center">
+                <span class="text-gray-600 font-semibold text-lg mr-1">{{ customer.id }}-</span>
+                <span class="text-gray-600 font-semibold text-lg">{{ customer.name }}</span>
+              </div>
+              <div
+                class="border-2 w-1/3 rounded-3xl px-3 py-1 flex justify-center items-center"
+                :class="{
+                  'border-green-400': customer.isProcessed == true,
+                  'border-red-600': customer.isProcessed !== true
 
-      <ul>
-        <li v-for="customer in customers" :key="customer.id">
-          {{ customer.id }}: {{ customer.name }} -- {{ customer }}
-        </li>
-      </ul>
+                }"
+              >
+                <span class="text-gray-600 text-base font-semibold" v-if="customer.isProcessed == true">isProcessed</span>
+                <span class="text-gray-600 text-base font-semibold" v-if="customer.isProcessed !== true">waiting</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Employee -->
+        <div class="w-1/2 px-16">
+          <div class="flex items-center justify-center my-8">
+            <span class="text-gray-600 font-semibold text-xl">Employees</span>
+          </div>
+          <div
+            class="w-full border-gray-600 border-2 px-6 py-4 rounded-lg mt-4"
+            v-for="employee in employees"
+            :key="employee.id"
+          >
+            <div class="w-full flex justify-between items-center">
+              <div class="flex items-center">
+                <span class="text-gray-600 font-semibold text-lg mr-1">{{ employee.id }}-</span>
+                <span class="text-gray-600 font-semibold text-lg">{{ employee.name }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div>
-      <span>Employees</span>
-
-      <ul>
-        <li v-for="employee in employees" :key="employee.id">
-          {{ employee.id }}: {{ employee.name }} -- {{ employee }}
-        </li>
-      </ul>
-    </div>
+    
   </div>
 </template>
 
